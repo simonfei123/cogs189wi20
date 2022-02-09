3b3fe019dd61d106acd7e1fab9ad0b90a30f95be
simonfei123 <tfei@ucsd.edu>
2022-02-09 04:31:09 -0800

    Test Python Version 1 Added the python version pipeline similar to matlab. But the CSP functions are still not working yet.

---
+++ A3/python/A3.py
@@ -0,0 +1,34 @@
+"""Skeleton for COGS 189 A3
+
+Please refer to this file when the Google Doc asks you to.
+Do not modify the names of variables given to you as that will cause
+issues with autograding.
+
+You will be uploading this file to Gradescope once you are done so 
+make sure everything is executable and nothing crashes before you submit
+the assignment."""
+
+
+VAR_NAME = "First Last"
+VAR_PID  = 'A000000'
+
+# --------------------------------------------------------------------------
+# Q1 -- What are the four classes present in this dataset?
+# Change the string to your answer.
+Q1_ANS = 'Enter your answer here as a string.'
+
+# --------------------------------------------------------------------------
+# Q2 -- Simple "Cross-Validation"
+
+# Goal: Create a 5-fold "CV" loop
+Q2_data = [1, 2, 3, 4, 5]
+
+# Create a for loop which takes Q2_data and
+# prints two things every iteration:
+#  1. The number of our iterator
+#  2. Every other number in Q2_data
+
+# Use i as your iterating variable
+
+
+
